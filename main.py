5ae2a908f03f6c43f377c03e97924dac02143ea6
ferchuk.iurii@pml.if.ua <ferchuk.iurii@pml.if.ua>
2023-10-28 17:40:44 +0300

    made game

--- main.py
+++ main.py
@@ -14,11 +14,100 @@ window.fill(backround_color)
 clock = pygame.time.Clock()
 
 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, filename, coords, size):
+        self.image = pygame.transform.scale(pygame.image.load(filename),
+                                        size)
+
+
+        self.rect = self.image.get_rect()
+        self.rect.center = coords 
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y ))
+
+class Racket(pygame.sprite.Sprite):
+    def __init__(self, filename, coords, size, speed):
+        self.image = pygame.transform.scale(
+                    pygame.image.load(filename),
+                    size
+        )
+        self.rect = self.image.get_rect()
+        self.rect.center = coords
+
+        self.speed = speed
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y ))
+
+
+    def update_l(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+
+
+    def update_r(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed        
+
+
+
+racket_l = Racket("tennis.png", (50, HEIGHT/2 ), (75,200), 5 )
+
+racket_r = Racket("tennis.png", (WIDHT-50, HEIGHT/2 ), (75,200), 5 )
+
+ball = Ball("tennis-ball.png", (WIDHT/2, HEIGHT/2), (50,50))
+
+speed_x, speed_y = 5,5
+
+pygame.font.init()
+font1 = pygame.font.Font(None, 60)
+
+
+
+
 game = True 
+finish = False
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False 
+
+    if not finish:
+        window.fill(backround_color)
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        ball.reset()
+
+        racket_l.reset()
+        racket_r.reset()
+
+        racket_l.update_l()
+        racket_r.update_r()
+
+        if ball.rect.y <= 0 or ball.rect.y >= HEIGHT - 50:
+            speed_y = -speed_y
+
+        if pygame.sprite.collide_rect(ball, racket_l) \
+        or  pygame.sprite.collide_rect(ball, racket_r):
+            speed_x = -speed_x
+
+        if ball.rect.x >= WIDHT+50:
+            finish = True
+            text = font1.render("Переміг Лівий Гравець",True,(0,0,100))
+            window.blit(text, (WIDHT/2, HEIGHT/2))
+
+        if ball.rect.x <= -50:
+            finish = True
+            text = font1.render("Переміг Правий Гравець",True,(0,0,100))
+            window.blit(text, (WIDHT/2, HEIGHT/2))
 
 
     pygame.display.update()
